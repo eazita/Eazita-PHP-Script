cdcb479e6dde8ff5125e31bd21fa53a639cd8644
Hamza Iqbal <hamza.hamza554@gmail.com>
2018-03-31 13:03:20 +0500

    Update Inbound.php

--- Inbound.php
+++ Inbound.php
@@ -6,8 +6,9 @@ $message=$_POST[message];   // ex Message will here without keyword
 $date=$_POST[date];         // ex 2014-11-29 13:51:42
 $keyword=$_POST[keyword];   // ex SMS keyword if exist
 
-// This is optional to send response message to user
-// Remember auto response message is not more than 2 page
+// This is optional to send response message to user.
+// Remember auto response message should not more than 1 page.
+// This feature is not supported for all accounts/inbound numbers.
 $responce['respond']="Hi this is a test msg\nThis text is on a new line";
 
 // Converting auto response message into JSON

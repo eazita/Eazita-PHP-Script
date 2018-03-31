82a21e02030ee0bf9c5035aa677562808f6a3e8d
Hamza Iqbal <hamza.hamza554@gmail.com>
2018-03-31 13:01:13 +0500

    Update Inbound.php

--- Inbound.php
+++ Inbound.php
@@ -1,10 +1,10 @@
 <?php
 // Get HTTP POST Message Parameters
-$from=$_POST[from];         // ex +923122699633
+$from=$_POST[from];         // ex 923122699633
 $to=$_POST[to];             // ex 923121103793
 $message=$_POST[message];   // ex Message will here without keyword
 $date=$_POST[date];         // ex 2014-11-29 13:51:42
-$keyword=$_POST[keyword];   // ex Test
+$keyword=$_POST[keyword];   // ex SMS keyword if exist
 
 // This is optional to send response message to user
 // Remember auto response message is not more than 2 page
